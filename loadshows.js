c5edc9a8d620bd151b5cdc405ca4b4ca449e45c3
derrick <dcravenus@gmail.com>
2015-11-18 15:24:14 -0700

    Initial commit

---
+++ loadshows.js
@@ -0,0 +1,43 @@
+var shows = [];
+var shows_loaded = 0;
+var show_ids = [
+    290, 732, 1866, 396, 49, 80
+];
+
+for (var i=0; i<show_ids.length; i++) {
+    var id = show_ids[i];
+    url = 'http://api.tvmaze.com/shows/' + id + '?embed[]=nextepisode&embed[]=previousepisode'
+
+    $.ajax({
+        url : url
+    }).done(function(data){
+        shows.push(data);
+        shows_loaded++;
+        if (shows_loaded==show_ids.length) {
+            renderShows();
+        }
+    });
+}
+
+var sortShows = function() {
+    shows.sort(function(a,b){
+        if (a.name < b.name) {
+            return -1;
+        }
+        if (a.name > b.name) {
+            return 1;
+        }
+        return 0;
+    });
+};
+
+var renderShows = function(){
+    sortShows();
+    for (var i=0; i<shows.length; i++) {
+        var template = $('#template').html();
+        Mustache.parse(template);
+
+        var rendered = Mustache.render(template, shows[i]);
+        $('#shows').append(rendered);
+    }
+};
